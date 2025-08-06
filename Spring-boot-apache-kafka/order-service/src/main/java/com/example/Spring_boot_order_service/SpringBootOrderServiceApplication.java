eae6ea943a1da799ed8b36c6210c9bc8c372ccaa
Sahiru22 <sahirubalasuriya222@gmail.com>
2025-08-06 16:21:15 +0530

    first commit

---
+++ Spring-boot-apache-kafka/order-service/src/main/java/com/example/Spring_boot_order_service/SpringBootOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_boot_order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOrderServiceApplication.class, args);
+	}
+
+}
---
+++ Spring-boot-apache-kafka/order-service/src/test/java/com/example/Spring_boot_order_service/SpringBootOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_boot_order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
